3486274b61d8c39806e454df9a785114df8939ad
BanMing <banming729@163.com>
2020-04-21 09:01:44 +0800

    mathutil

---
+++ 3dMathBook/README.md
@@ -0,0 +1,6 @@
+# 3dMathBook
+ 3d数学基础书测试代码
+
+
+## 笔记
+[思维导图](https://edrawcloudpubliccn.oss-cn-shenzhen.aliyuncs.com/viewer/self/16747446/share/2020-4-21/1587428841/main.svg)
---
+++ 3dMathBook/header/MathUtil .h
@@ -0,0 +1,41 @@
+/******************************************************************
+** 文件名: MathUtil.h
+** 创建人: BanMing 
+** 日  期: 4/21/2020, 8:32:19 AM
+** 描  述:  
+
+**************************** 修改记录 ******************************
+** 修改人: 
+** 日  期: 
+** 描  述: 
+*******************************************************************/
+
+// #ifdef __MATHUTIL_H_INCLUDED__
+#define __MATHUTIL_H_INCLUDED__
+
+#include <math.h>
+
+// 定义和Pi相关的常量
+const float kPi = 3.14159265f;
+const float k2Pi = kPi * 2.0f;
+const float kPiOver2 = kPi / 2.0f;
+const float k1OverPi = 1.0f / kPi;
+const float k1Over2Pi = 1.0f / k2Pi;
+
+// 通过加适当的2pi倍数将角度限制在-pi到pi的区间内
+extern float wrapPi(float theta);
+
+// “安全”反三角函数
+extern float safeAcos(float x);
+
+// 计算角度的sin和cos值
+// 在某些平台上，如果需要这两个值，同时计算要比分开计算快
+inline void sinCos(float *returnSin, float *returnCos, float theta)
+{
+    // 为了简单，我们只使用标准三角函数
+    // 注意在某些平台上可以做的更好一些
+    *returnCos = cos(theta);
+    *returnSin = sin(theta);
+}
+
+// #endif
